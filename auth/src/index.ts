7544183f901a6ad43b7f9c97880617ab212daa70
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-30 16:50:33 -0400

    Adding Error Handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import { json } from "body-parser";
+import { currentuserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
-app.use(json())
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send('Hi there!')
-})
+app.use(json())
+app.use(currentuserRouter,signinRouter,signoutRouter,signupRouter)
+app.use(errorHandler)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!!!')
