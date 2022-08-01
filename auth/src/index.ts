74311df90bece37ea8d3599a9a6bc988cf9b26e3
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-08-01 01:28:21 -0400

    Added password hashing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import 'express-async-errors'
+import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
 
@@ -12,15 +12,26 @@ import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
-app.use(json())
-app.use(currentuserRouter, signinRouter, signoutRouter, signupRouter)
+app.use(json());
+app.use(currentuserRouter, signinRouter, signoutRouter, signupRouter);
 
-app.all('*',async () => {
-  throw new NotFoundError()
-})
+app.all("*", async () => {
+  throw new NotFoundError();
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!!')
-})
+  console.log("Listening on port 3000!!!");
+});
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,28 +1,39 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Email must be valid'),
-  body('password')
-    .trim()
-    .isLength({ min: 4, max: 20 })
-  .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
-  const errors = validationResult(req)
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+    console.log("Created New User");
+    res.status(201).send(user);
   }
+);
 
-  const { email, password } = req.body
-
-  res.status(201).send({})
-})
-
-export {router as signupRouter}
+export { router as signupRouter };
