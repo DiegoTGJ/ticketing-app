8c74a3ebf19289faa2abd9fbcd37e7d9ad1b7751
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2022-08-02 16:23:06 -0400

    refactor requests, added ssr for auth

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,27 @@
-const Index = () => {
-  return <h1>señora alicia</h1>;
+import axios from "axios";
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing Page</h1>;
 };
 
-export default Index;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // we are on the server -> change host to https://ingress..
+    const { data } = await axios
+      .get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        {
+          headers: req.headers,
+        }
+      )
+      .catch((err) => console.log(err.message));
+    return data;
+  } else {
+    //we are on the browser! -> make request with host of the app
+    const { data } = await axios
+      .get("/api/users/currentuser")
+      .catch((err) => console.log(err.message));
+    return data;
+  }
+};
+export default LandingPage;
