9a1a86ca9002402bb5d28519b94a96a0e01bdef9
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2022-08-02 16:23:47 -0400

    refactor requests to api's

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import axios from "axios";
-
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
   const onSubmit = async (event) => {
     event.preventDefault();
-
-    try {
-      const response = await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    await doRequest();
   };
 
   return (
@@ -40,16 +41,7 @@ const Signup = () => {
             className="form-control mb-3"
           />
         </div>
-        {errors.length > 0 && (
-          <div className="alert alert-danger">
-            <h4>Ooops..</h4>
-            <ul className="my-0">
-              {errors.map((err) => (
-                <li key={err.message}>{err.message}</li>
-              ))}
-            </ul>
-          </div>
-        )}
+        {errors}
         <button className="btn btn-primary">Sign Up</button>
       </form>
     </div>
