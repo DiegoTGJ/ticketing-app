4695f845ea99beac9e681a7c1d73111359eb31d7
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2022-08-03 02:59:12 -0400

    Added create ticket endpoint

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../model/ticket";
 
 const route = "/api/tickets";
 
@@ -11,8 +12,65 @@ it("can only be accessed if the user is signed in", async () => {
   await request(app).post("/api/tickets").send({}).expect(401);
 });
 it("returns status 201 if user is signed in", async () => {
-  await request(app).post("/api/tickets").send({}).expect(201);
+  await request(app)
+    .post(route)
+    .set("Cookie", signin())
+    .send({
+      title: "asdasd",
+      price: 11,
+    })
+    .expect(201);
 });
-it("returns an error if an invalid title is provided", async () => {});
-it("returns an error if an invalid price is provided", async () => {});
-it("creates a ticket with valid input", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+  await request(app)
+    .post(route)
+    .set("Cookie", signin())
+    .send({
+      title: "",
+      price: 10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post(route)
+    .set("Cookie", signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post(route)
+    .set("Cookie", signin())
+    .send({
+      title: "Rock and Metal",
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post(route)
+    .set("Cookie", signin())
+    .send({
+      title: "Rock and Metal",
+    })
+    .expect(400);
+});
+it("creates a ticket with valid input", async () => {
+  // add check ticket is saved to database
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+  await request(app)
+    .post(route)
+    .set("Cookie", signin())
+    .send({
+      title: "Rock and Metal",
+      price: 10,
+    })
+    .expect(201);
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(10);
+  expect(tickets[0].title).toEqual("Rock and Metal");
+});
