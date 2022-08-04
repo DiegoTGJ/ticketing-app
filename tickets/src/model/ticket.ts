8a2c383f4d6ea4c369e99d15f3cdaa5ca4c56c76
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2022-08-04 14:29:40 -0400

    added missing export

--- tickets/src/model/ticket.ts
+++ tickets/src/model/ticket.ts
@@ -48,4 +48,4 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
 
-export { Ticket };
+export { Ticket, TicketAttrs };
